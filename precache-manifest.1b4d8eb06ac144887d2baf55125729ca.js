7f18f3f74f4423dd891018b939b0383744c0ac9d
David L <dongxli710@gmail.com>
2019-05-01 23:37:26 -0700

    Built

---
+++ precache-manifest.1b4d8eb06ac144887d2baf55125729ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b153e842bf69a1d806d8",
+    "url": "/static/js/main.723c2432.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b153e842bf69a1d806d8",
+    "url": "/static/css/main.e5654283.chunk.css"
+  },
+  {
+    "revision": "1b2fa9a8a9fd1ffe7cee019446d89a44",
+    "url": "/index.html"
+  }
+];
