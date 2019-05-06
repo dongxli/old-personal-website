0a5c4d70d802cf1437970b45ee68d283f9f38591
David L <dongxli710@gmail.com>
2019-05-05 22:03:11 -0700

    Updated data.json

--- Current version/src/App.js
+++ Current version/src/App.js
@@ -8,8 +8,6 @@ import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 import data from "./data.json";
 
-// import "../node_modules/@fortawesome";
-
 class App extends Component {
   componentWillMount() {
     this.setState({ data: data });
